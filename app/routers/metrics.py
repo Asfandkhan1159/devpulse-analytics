14c9ae6af76554d61fdc7c9fe1d3586f178a1d87
AsfandKhan1159 <asfand.khan5024@gmail.com>
2026-04-09 01:18:10 +0500

    All dora metrics with real db queries,handle 404 for missing project, fix frequency label for 0 deployment

--- app/routers/metrics.py
+++ app/routers/metrics.py
@@ -1,59 +1,89 @@
 
-from datetime import datetime
+from datetime import datetime, timezone
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 
 from app.schemas.metrics import ChangeFailureRateResponse, DeploymentFrequencyResponse, LeadTimeResponse, MeanTimeToRecoveryResponse 
-from app.services.metrics_services import calculate_deployment_frequency
+from app.services.metrics_services import  calculate_cutoff, calculate_deployment_frequency, calculate_lead_time, calculate_change_failure_rate,calculate_mttr
 from app.db.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.models.events import Project
 
 router = APIRouter()
+
+#helper function to check if project exists
+def check_project_exists(project_id: int, db: Session):
+    project = db.execute(select(Project).filter_by(id=project_id)).scalar_one_or_none()
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return project
 
 
 
 @router.get("/deployment-frequency", response_model=DeploymentFrequencyResponse)
 def calculate_metrics(project_id: int, days: int = 30, db: Session = Depends(get_db)):
+    check_project = check_project_exists(project_id, db)
+    
     # Placeholder for actual metrics calculation logic
     # In a real implementation, you would calculate the metrics based on the input data
-    metrics = calculate_deployment_frequency( project_id, days,db)
+    cutoff_date = calculate_cutoff(days)
+    metrics = calculate_deployment_frequency(project_id, cutoff_date, db)
 
     return DeploymentFrequencyResponse(
         project_id=project_id,
         period_days=days,
         calculated_at=datetime.utcnow(),
         total_deployments=metrics["total_deployments"],
-        daily_average=metrics["daily_average"]
+        daily_average=metrics["daily_average"],
+        frequency_label=metrics["frequency_label"]
     )
 
 @router.get("/lead-time", response_model=LeadTimeResponse)
-def calculate_lead_time(project_id: int, days: int = 30):
+def get_lead_time(project_id: int,days: int = 30 ,db: Session = Depends(get_db)):
+    check_project = check_project_exists(project_id, db)
+    
+    # Calculate the cutoff date
+    cutoff_date = calculate_cutoff(days)
+    metrics = calculate_lead_time(project_id, db, cutoff_date)
 
     return LeadTimeResponse(
         project_id=project_id,
         period_days=days,
         calculated_at=datetime.utcnow(),
-        average_lead_time_hours=5.5  # Example value
+        average_lead_time_hours=metrics["average_lead_time_hours"]
     )
 
 @router.get("/change-failure-rate", response_model=ChangeFailureRateResponse)
-def calculate_change_failure_rate(project_id: int, days: int = 30):
+def get_change_failure_rate(project_id: int, days: int = 30, db: Session = Depends(get_db)):
+    check_project = check_project_exists(project_id, db)
+   
+    # Calculate the cutoff date
+    cutoff_date = calculate_cutoff(days)
+    metrics = calculate_change_failure_rate(project_id, db, cutoff_date)
 
     return ChangeFailureRateResponse(
         project_id=project_id,
         period_days=days,
         calculated_at=datetime.utcnow(),
-        failure_rate_percentage=12.5  # Example value
+        failure_rate_percentage=metrics["failure_rate_percentage"]
     )
 
 @router.get("/mean-time-to-recovery", response_model=MeanTimeToRecoveryResponse)
-def calculate_mean_time_to_recovery(project_id: int, days: int = 30):
+def get_mean_time_to_recovery(project_id: int, days: int = 30, db: Session = Depends(get_db)):
+    check_project = check_project_exists(project_id, db)
+    
+    # Calculate the cutoff date
+    cutoff_date = calculate_cutoff(days)
+    metrics = calculate_mttr(project_id, db, cutoff_date)
+    print("printing mttr", metrics["avg_mttr"])
+
 
     return MeanTimeToRecoveryResponse(
         project_id=project_id,
         period_days=days,
         calculated_at=datetime.utcnow(),
-        mean_time_to_recovery_hours=8.0  # Example value
+        avg_mttr=metrics["avg_mttr"]
     )
 
--- app/services/metrics_services.py
+++ app/services/metrics_services.py
@@ -4,14 +4,90 @@ from sqlalchemy import select,and_, func
 from app.models.events import Event, Project
 
 
-def calculate_deployment_frequency(project_id:int, days:int,db: Session):
-    cutoff = datetime.now(timezone.utc) - timedelta(days=30)
-    result = select(func.count(Event.id)).where(and_(Event.project_id == project_id, Event.event_type == "pipeline", Event.timestamp >= cutoff))
+def calculate_frequency_label(daily_average) -> str:
+    if daily_average >= 1:
+        return "Multiple Deployments per Day"
+    elif daily_average == 0:
+        return "No deployments"
+    
+    elif daily_average >= 0.14:
+        return "Multiple Deployments per Week"
+    elif daily_average >= 0.07:
+        return "Once per Week"
+    elif daily_average < 0.07:
+        return "Once per month"
+    
+def base_pipeline_query(project_id:int, cutoff: datetime):
+    
+    return select(Event).where(and_(
+        Event.project_id == project_id,
+        Event.event_type == "pipeline",
+        Event.timestamp >= cutoff
+    ))    
+
+def calculate_cutoff(days:int):
+    return datetime.now(timezone.utc) - timedelta(days=days)
+
+def calculate_deployment_frequency(project_id:int, cutoff: datetime, db: Session):
+    
+    query = base_pipeline_query(project_id, cutoff)
+    result = select(func.count()).select_from(query.subquery())
     total_deployments = db.execute(result).scalar()
 
+    # Calculate the number of days in the period
+    days = (datetime.now(timezone.utc) - cutoff).days or 1
     daily_average = total_deployments / days
+    frequency_label = calculate_frequency_label(daily_average)
+    print("frquency_label", frequency_label)
 
     return {
         "total_deployments": total_deployments,
-        "daily_average": daily_average
+        "daily_average": daily_average,
+        "frequency_label": frequency_label
+    }
+
+def calculate_lead_time(project_id:int, db:Session, cutoff: datetime):
+    #average time between created at and finished at for successful merge request events
+    
+    result = select(func.avg(func.extract('epoch',Event.finished_at - Event.created_at)/3600)).where(and_(Event.project_id == project_id, Event.event_type == "merge_request", Event.status == "success", Event.timestamp >= cutoff))
+    average_lead_time_hours = db.execute(result).scalar() or 0.0
+
+    return {
+        "average_lead_time_hours": average_lead_time_hours
+    }
+
+def calculate_change_failure_rate(project_id:int, db:Session, cutoff: datetime):
+    query = base_pipeline_query(project_id, cutoff)
+    total_deployments_result = select(func.count()).select_from(query.subquery())
+    total_deployments = db.execute(total_deployments_result).scalar()
+    failed_deployments_result = select(func.count(Event.id)).where(and_(Event.project_id == project_id, Event.event_type == "pipeline", Event.status == "failed", Event.timestamp >= cutoff))
+    failed_deployments = db.execute(failed_deployments_result).scalar()
+    failure_rate_percentage = (failed_deployments / total_deployments) * 100 if total_deployments > 0 else 0
+
+    return {
+        "failure_rate_percentage": failure_rate_percentage
+    }
+
+
+def calculate_mttr(project_id: int, db: Session, cutoff: datetime):
+    # find all failed events
+    failed_event_result = select(Event).where(and_(Event.project_id == project_id, Event.event_type == "pipeline", Event.status == "failed", Event.timestamp >= cutoff)).order_by(Event.timestamp)
+    failed_event = db.execute(failed_event_result).scalars().all()
+    # for each failure, find the next success after it
+    recovery_times = []
+    for event in failed_event:
+        recovery = db.execute(select(Event).where(and_(
+            Event.project_id == project_id,
+            Event.event_type == "pipeline",
+            Event.status == "success",
+            Event.created_at > event.finished_at
+        )).order_by(Event.created_at).limit(1)).scalar_one_or_none() 
+        if recovery:
+            diff = (recovery.created_at - event.created_at).total_seconds() / 3600  # Convert to hours
+            recovery_times.append(diff)
+    # calculate average time difference
+    avg_mttr = sum(recovery_times) / len(recovery_times) if recovery_times else 0
+
+    return {
+        "avg_mttr": avg_mttr
     }
